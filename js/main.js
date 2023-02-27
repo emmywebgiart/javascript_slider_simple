1b99f446a3fc36608cecdbd7fddb520f72e4c94e
Emmanuel Angeles <eangels1595@gmail.com>
2023-02-27 01:31:53 -0600

    Feat: Agregar contadores de lista total de imagenes e indice de imagen activa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let image = document.getElementById("image");
 let buttonPrev = document.getElementById("button_prev");
 let buttonNext = document.getElementById("button_next");
+let totalImg = document.getElementById("total_img");
+let currentImg = document.getElementById("current_img");
 let position = 0;
 const urlsImages = [
     "./img/img_logo_html.png",
@@ -10,17 +12,21 @@ const urlsImages = [
 ]
 let lengthImages = urlsImages.length;
 
-image.src = urlsImages[0];
+image.src = urlsImages[position];
+currentImg.innerHTML = position + 1;
+totalImg.innerHTML = lengthImages;
 console.log(lengthImages)
 
 buttonPrev.onclick = () => {
     image.src = urlsImages[(position - 1 + lengthImages) % lengthImages];
     position = (position - 1 + lengthImages) % lengthImages;
+    currentImg.innerHTML = position + 1;
 }
 
 buttonNext.onclick = () => {
     image.src = urlsImages[(position + 1) % lengthImages];
     position = (position + 1) % lengthImages;
+    currentImg.innerHTML = position + 1;
 }
 
 
